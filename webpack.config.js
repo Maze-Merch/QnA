65c593f81ff9cc4e0bfb3d4072e179ea8e4477aa
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2020-06-16 20:04:01 -0600

    Matt Commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './client/src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./client/src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
