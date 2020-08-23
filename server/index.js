fd8b98bc5aaefd62ec1c4f91cc0f8e9d57c82927
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2020-08-23 12:36:42 -0600

    updating port for heroku deploy

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3006;
+const PORT = process.env.PORT || 3006;
 app.use(cors());
 app.use(express.static('public'));
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Listening at http://localhost:${port}`));
