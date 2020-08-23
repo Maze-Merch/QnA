6bfac5b7f0c02076422c275892024264d11d1eb6
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2020-08-23 14:09:16 -0600

    heroku deploy

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -59,7 +59,7 @@ class App extends Component {
   productFetcher(){
     fetch('//52.26.193.201:3000/qa/5?count=1000')
     .then(response=> response.json())
-    .then(data => this.setState({questions:data}))
+    .then(data => this.setState({questions:data}), console.log(data))
   }
 
   componentDidMount(){
