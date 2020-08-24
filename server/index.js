e756aeba86128f6ddf49b364ce9d5c47281c7347
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2020-08-24 08:43:36 -0600

    heroku fetch issues

--- server/index.js
+++ server/index.js
@@ -4,9 +4,16 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3006;
 app.use(cors());
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect(`http://${req.hostname}${req.url}`);
+  } else {
+    next();
+  }
+});
 app.use(express.static(`${__dirname}/../public`));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`listening on port ${PORT}`);
